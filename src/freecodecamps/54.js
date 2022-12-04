635dc676117b6374154097baeb6fb441118acb6a
Ni-speed <railmir@gmail.com>
2022-12-04 21:24:38 +0500

    fcc54

---
+++ src/freecodecamps/54.js
@@ -0,0 +1,10 @@
+function myLocalScope() {
+    const myVar = 1;
+
+    console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
