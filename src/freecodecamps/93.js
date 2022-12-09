a13ab892f9ca62a610e4b72d8a18cec341815d2e
Ni-speed <railmir@gmail.com>
2022-12-09 21:26:44 +0500

    fcc93

---
+++ src/freecodecamps/93.js
@@ -0,0 +1,23 @@
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true,
+    }, {
+        "artist": "1",
+        "title": "2",
+        "release_year": 1979,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true,
+    }
+];
