85fde643edf9b569a310f2508fd9c4f6f327154d
Ni-speed <railmir@gmail.com>
2022-12-17 16:58:27 +0500

    ES18

---
+++ src/freecodecamps/ES6/19.js
@@ -0,0 +1,19 @@
+// Only change code below this line
+class Thermostat {
+    constructor(temp) {
+        this._temp = temp;
+    }
+    get temperature() {
+        return 5/9 * (this._temp - 32);
+    }
+    set temperature(dTemperature) {
+        this._temp = dTemperature * 9.0 / 5 + 32;
+    }
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
+console.log(temp)
